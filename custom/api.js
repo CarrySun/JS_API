ec35432ac112a32ba1fc911847e837224d7022b9
CarrySun2018 <CarrySun2018@163.com>
2018-02-24 19:42:30 +0800

    深拷贝

--- custom/api.js
+++ custom/api.js
@@ -33,7 +33,29 @@ function guid() {
     S4()
   );
 }
-
+/**
+ *对象数组的深拷贝
+ */
+function clone(Obj) {
+  var buf;
+  if (Obj instanceof Array) {
+    buf = []; //创建一个空的数组
+    var i = Obj.length;
+    while (i--) {
+      buf[i] = clone(Obj[i]);
+    }
+    return buf;
+  } else if (Obj instanceof Object) {
+    buf = {}; //创建一个空对象
+    for (var k in Obj) {
+      //为这个对象添加新的属性
+      buf[k] = clone(Obj[k]);
+    }
+    return buf;
+  } else {
+    return Obj;
+  }
+}
 /**
  * 数组的深拷贝
  */
